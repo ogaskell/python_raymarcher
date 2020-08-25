628c098fc3e38edafe5a7da1ddada4a4572c0049
Oliver Gaskell <olivergaskell700@gmail.com>
2020-08-25 14:30:07 +0100

    Linting

--- test_render.py
+++ test_render.py
@@ -8,31 +8,31 @@ from raymarcher.raymarch import Renderer
 
 params = sys.argv
 if len(sys.argv) == 1:
-    OUTPUT_WIDTH, OUTPUT_HEIGHT = 320,240
+    OUTPUT_WIDTH, OUTPUT_HEIGHT = 320, 240
     BACKGROUND = 0
 
 if len(sys.argv) == 3:
-    OUTPUT_WIDTH, OUTPUT_HEIGHT = map(int,sys.argv[1].split(","))
-    BACKGROUND = map(int,sys.argv[2].split(","))
+    OUTPUT_WIDTH, OUTPUT_HEIGHT = map(int, sys.argv[1].split(","))
+    BACKGROUND = map(int, sys.argv[2].split(","))
 
 elif len(sys.argv) == 4:
-    OUTPUT_WIDTH, OUTPUT_HEIGHT = map(int,sys.argv[1].split(","))
-    BACKGROUND = map(int,sys.argv[2].split(","))
+    OUTPUT_WIDTH, OUTPUT_HEIGHT = map(int, sys.argv[1].split(","))
+    BACKGROUND = map(int, sys.argv[2].split(","))
     FILENAME = sys.argv[3]
 
-#define shapes
-shapes = [Sphere(center = Vector3(0,0,5),
-                 radius = 1),
-          #Sphere(center = Vector3(3,-1,8),
-          #       radius = 0.6,
-          #       color=255),
-          #Sphere(center = Vector3(2,2,10),
-          #       radius = 1.2,
-          #       color=60),
+# define shapes
+shapes = [Sphere(center=Vector3(0, 0, 5),
+                 radius=1),
+          # Sphere(center = Vector3(3,-1,8),
+          #        radius = 0.6,
+          #        color=255),
+          # Sphere(center = Vector3(2,2,10),
+          #        radius = 1.2,
+          #        color=60),
           ]
 
-#render
-engine = Renderer((OUTPUT_HEIGHT,OUTPUT_WIDTH),
+# render
+engine = Renderer((OUTPUT_HEIGHT, OUTPUT_WIDTH),
                   BACKGROUND,
                   shapes)
 
@@ -40,6 +40,6 @@ print("Beginning render")
 image = engine.render()
 print(image)
 
-#create image
+# create image
 pil_img = Image.fromarray(image)
 pil_img.show()
