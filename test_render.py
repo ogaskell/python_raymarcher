563085e269175a2dba7cd1d5d91891717e3892d2
Oliver Gaskell <olivergaskell700@gmail.com>
2020-08-25 13:54:49 +0100

    Moved rendering test code from raymarch.py to separate file.
    This makes it much easier to read the testing/rendering code as they're 
    now separate.

---
+++ test_render.py
@@ -0,0 +1,47 @@
+import numpy as np
+from PIL import Image
+import sys
+from tqdm import tqdm
+
+from shapes import *
+from tools import *
+
+from raymarch import *
+
+params = sys.argv
+if len(sys.argv) == 1:
+    OUTPUT_WIDTH, OUTPUT_HEIGHT = 320,240
+    BACKGROUND = 0
+
+if len(sys.argv) == 3:
+    OUTPUT_WIDTH, OUTPUT_HEIGHT = map(int,sys.argv[1].split(","))
+    BACKGROUND = map(int,sys.argv[2].split(","))
+
+elif len(sys.argv) == 4:
+    OUTPUT_WIDTH, OUTPUT_HEIGHT = map(int,sys.argv[1].split(","))
+    BACKGROUND = map(int,sys.argv[2].split(","))
+    FILENAME = sys.argv[3]
+
+#define shapes
+shapes = [Sphere(center = Vector3(0,0,5),
+                 radius = 1),
+          #Sphere(center = Vector3(3,-1,8),
+          #       radius = 0.6,
+          #       color=255),
+          #Sphere(center = Vector3(2,2,10),
+          #       radius = 1.2,
+          #       color=60),
+          ]
+
+#render
+engine = Renderer((OUTPUT_HEIGHT,OUTPUT_WIDTH),
+                  BACKGROUND,
+                  shapes)
+
+print("Beginning render")
+image = engine.render()
+print(image)
+
+#create image
+pil_img = Image.fromarray(image)
+pil_img.show()
